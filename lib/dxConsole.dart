0f54e9a5a84ff24866545881abcff4e4fd45d32d
George Aslanis <osstekz@gmail.com>
2014-09-29 00:21:10 -0400

    v0.8.0

---
+++ lib/dxConsole.dart
@@ -0,0 +1,35 @@
+library dxConsole;
+
+import "dart:async";
+import "dart:io";
+import "dart:isolate";
+import "dart:typed_data";
+import "package:ansicolor/ansicolor.dart";
+//import "package:osslib/ossutils.dart";
+//import "package:ossdevtools/utils.dart";
+
+//===================================================
+//SECTION: Native extensions
+//REFER:https://www.dartlang.org/articles/native-extensions-for-standalone-dart-vm/#building-on-windows
+//SOURCE:http://dart.googlecode.com/svn/trunk/dart/samples/sample_extension/
+import "dart-ext:bin/dxConsole";
+//import 'dart:nativewrappers' as nw;
+//===================================================
+
+part 'src/dxconsole_input.dart';
+part 'src/dxconsole_main.dart';
+
+class SysInfo {
+//TODO:PERF:Combine SysInfo into single call native "XTGetSysInfo";
+	static final bool isLittleEndian = _isLittleEndian();
+	static bool _isLittleEndian() native "XTIsLittleEndian";
+
+	static final int pageSize = _getPageSize();
+	static int _getPageSize() native "XTGetPageSize";
+
+	static final int sizeOfInt = _getSizeOfInt();
+	static int _getSizeOfInt() native "XTGetSizeOfInt";
+
+	static final String version = _getVersionString();
+	static String _getVersionString() native "XTGetVersionString";
+}
